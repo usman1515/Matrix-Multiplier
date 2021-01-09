d9a6de5cf7e4b73fc246bde3e16ee845c5138b4d
usmans1515 <usmansiddique1515@gmail.com>
2021-01-09 23:56:18 +0500

    updated script to compile and run all testbenches

--- run_verilator.sh
+++ run_verilator.sh
@@ -24,12 +24,37 @@ iverilog -Wall -o top_matrix_multiplier.o 				../rtl/top_matrix_multiplier.v
 echo "------> Compiled all verilog DUT files"
 
 #----------------------------------------------------------------- generating binaries for all TB files
+echo "------> Compiling all verilog TB files"
 
-# iverilog -Wall -o tb_carrylookahead_adder.o       ../test_bench/tb_carrylookahead_adder.v
-# vvp tb_carrylookahead_adder.o
+iverilog -Wall -o tb_carrylookahead_adder.o ../test_bench/tb_carrylookahead_adder.v
+vvp tb_carrylookahead_adder.o
 # gtkwave carrylookahead_adder.vcd
 
+iverilog -Wall -o tb_control_path.o   ../test_bench/tb_control_path.v
+vvp tb_control_path.o
+# gtkwave control_path.vcd
 
+iverilog -Wall -o tb_datapath.o       ../test_bench/tb_datapath.v
+vvp tb_datapath.o
+# gtkwave data_path.vcd
+
+iverilog -Wall -o tb_matrix_10x10.o   ../test_bench/tb_matrix_10x10.v
+vvp tb_matrix_10x10.o
+# gtkwave matrix_10x10.vcd
+
+iverilog -Wall -o tb_registers.o      ../test_bench/tb_registers.v
+vvp tb_registers.o
+# gtkwave registers.vcd
+
+iverilog -Wall -o tb_vedicmultiplier.o  ../test_bench/tb_vedicmultiplier.v
+vvp tb_vedicmultiplier.o
+# gtkwave vedic_multipliers.vcd
+
+iverilog -Wall -o top_matrix_multiplier.o   ../test_bench/tb_top_matrix_multiplier.v
+vvp top_matrix_multiplier.o
+# gtkwave top_matrix_multiplier.vcd
+
+#----------------------------------------------------------------- go back to root project directory and end script
 cd ..   # go back to project root directory
 
 # NOTE; remove all DUT files from dir. add one by one and make commits. same for TB files
