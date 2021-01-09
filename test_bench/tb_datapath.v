e2477d745a9698fe6e7694c6230059a36b66b09c
usmans1515 <usmansiddique1515@gmail.com>
2021-01-09 22:06:04 +0500

    updated data path TB

--- test_bench/tb_datapath.v
+++ test_bench/tb_datapath.v
@@ -1,3 +1,4 @@
+`include "../rtl/data_path.v"
 `timescale 1ns/100ps
 
 module tb_datapath;
@@ -6,33 +7,41 @@ localparam DATA_WIDTH=8;
 localparam T=2;
 localparam min='h0,max='ha;
 
-logic   clk;
-logic   reset_n;
-logic   [DATA_WIDTH-1:0] readData_A;
-logic   [DATA_WIDTH-1:0] readData_B;
-logic   en_Mux;
-logic   en_PPReg;
-logic   en_FDReg;
-logic   [DATA_WIDTH-1:0] writeData;
-logic   resultIsInvalid;
+//-------------------------------------------------------------------------- global signals
+reg   clk;
+reg   reset_n;
+//-------------------------------------------------------------------------- data and control signals
+reg   [DATA_WIDTH-1:0] inData_A;
+reg   [DATA_WIDTH-1:0] inData_B;
+reg   en_Mux;
+reg   en_PPReg;
+reg   en_FDReg;
+wire  [DATA_WIDTH-1:0] outData;
+wire  resultIsInvalid;
 
+//-------------------------------------------------------------------------- DUT instantiation
 data_path #(.DATA_WIDTH (DATA_WIDTH)) DATA_PATH(
   .clk             (clk             ),
   .reset_n         (reset_n         ),
-  .readData_A      (readData_A      ),
-  .readData_B      (readData_B      ),
+  .inData_A        (inData_A        ),
+  .inData_B        (inData_B        ),
   .en_Mux          (en_Mux          ),
   .en_PPReg        (en_PPReg        ),
   .en_FDReg        (en_FDReg        ),
-  .writeData_C     (writeData       ),
+  .outData         (outData         ),
   .resultIsInvalid (resultIsInvalid )
 );
 
-
+//-------------------------------------------------------------------------- clock generator
 always begin
     #(T/2) clk=1'b0;    #(T/2) clk=1'b1;
 end
+
+//-------------------------------------------------------------------------- main loop
 initial begin
+	$write("-----------------------------------------------------------------\n");
+  $write("                     Data Path Testbench                         \n");
+	$write("-----------------------------------------------------------------\n");
   @(posedge clk)
     reset_n=1'b0;
   @(posedge clk) begin
@@ -41,44 +50,54 @@ initial begin
     en_FDReg=1'b0;
   end
   repeat(3) multiply;
+	$write("-----------------------------------------------------------------\n");
   $finish;
 end
 
-task multiply;  
+//-------------------------------------------------------------------------- tasks
+task multiply; begin  
   @(posedge clk) begin
-    readData_A=$urandom_range(min,max);
-    readData_B=$urandom_range(min,max);
+    inData_A=$urandom_range(min,max);
+    inData_B=$urandom_range(min,max);
     en_Mux=1'b0;
     en_PPReg=1'b1;
     en_FDReg=1'b0;
-    $$display("Num_1= %3d  Num_2= %3d  en_Mux= %1b  en_PPReg= %1b  en_FDReg= %1b",
-                readData_A,readData_B,en_Mux,en_PPReg,en_FDReg);
+    $display("Num_1= %3d  Num_2= %3d  en_Mux= %1b  en_PPReg= %1b  en_FDReg= %1b",
+                inData_A,inData_B,en_Mux,en_PPReg,en_FDReg);
   end
   repeat(8) @(posedge clk) begin
-    readData_A=$urandom_range(min,max);
-    readData_B=$urandom_range(min,max);
+    inData_A=$urandom_range(min,max);
+    inData_B=$urandom_range(min,max);
     en_Mux=1'b1;
     en_PPReg=1'b1;
     en_FDReg=1'b0;
-    $$display("Num_1= %3d  Num_2= %3d  en_Mux= %1b  en_PPReg= %1b  en_FDReg= %1b",
-                readData_A,readData_B,en_Mux,en_PPReg,en_FDReg);
+    $display("Num_1= %3d  Num_2= %3d  en_Mux= %1b  en_PPReg= %1b  en_FDReg= %1b",
+                inData_A,inData_B,en_Mux,en_PPReg,en_FDReg);
   end
   @(posedge clk) begin
-    readData_A=$urandom_range(min,max);
-    readData_B=$urandom_range(min,max);
+    inData_A=$urandom_range(min,max);
+    inData_B=$urandom_range(min,max);
     en_Mux=1'b1;
     en_PPReg=1'b1;
     en_FDReg=1'b1;
-    $$display("Num_1= %3d  Num_2= %3d  en_Mux= %1b  en_PPReg= %1b  en_FDReg= %1b",
-                readData_A,readData_B,en_Mux,en_PPReg,en_FDReg);
+    $display("Num_1= %3d  Num_2= %3d  en_Mux= %1b  en_PPReg= %1b  en_FDReg= %1b",
+                inData_A,inData_B,en_Mux,en_PPReg,en_FDReg);
   end
   @(posedge clk) begin
     en_Mux=1'b0;
     en_PPReg=1'b0;
     en_FDReg=1'b0;
-    $$display("Num_1= %3d  Num_2= %3d  en_Mux= %1b  en_PPReg= %1b  en_FDReg= %1b",
-                readData_A,readData_B,en_Mux,en_PPReg,en_FDReg);
+    $display("Num_1= %3d  Num_2= %3d  en_Mux= %1b  en_PPReg= %1b  en_FDReg= %1b",
+                inData_A,inData_B,en_Mux,en_PPReg,en_FDReg);
   end
+end
 endtask
 
+//-------------------------------------------------------------------------- vcd output
+initial begin
+	$dumpfile("data_path.vcd");
+	$dumpvars(0,DATA_PATH);
+end
+
+//-------------------------------------------------------------------------- end of code
 endmodule
