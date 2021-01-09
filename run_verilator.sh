43d36fd7221fa8451be289085d4da64e272a6b7d
usmans1515 <usmansiddique1515@gmail.com>
2021-01-09 19:29:21 +0500

    script can now compile all verilog DUT files using verilator

--- run_verilator.sh
+++ run_verilator.sh
@@ -6,6 +6,7 @@ rm ./*    # remove all previous binary and vcd files
 
 #----------------------------------------------------------------- generating binaries for all DUT files
 echo "------> Compiling all verilog DUT files"
+
 iverilog -Wall -o half_adder.o 										../rtl/half_adder.v
 iverilog -Wall -o carry_lookaheadblock_4bit.o 		../rtl/carry_lookaheadblock_4bit.v
 iverilog -Wall -o carry_lookaheadadder_16bit.o		../rtl/carry_lookaheadadder_16bit.v
@@ -19,12 +20,16 @@ iverilog -Wall -o vedicmultiplier_8bit.o 					../rtl/vedicmultiplier_8bit.v
 iverilog -Wall -o control_path.o 									../rtl/control_path.v
 iverilog -Wall -o data_path.o 										../rtl/data_path.v
 iverilog -Wall -o top_matrix_multiplier.o 				../rtl/top_matrix_multiplier.v
+
 echo "------> Compiled all verilog DUT files"
 
 #----------------------------------------------------------------- generating binaries for all TB files
-iverilog -Wall -o tb_carrylookahead_adder.o       ../test_bench/tb_carrylookahead_adder.v
-vvp tb_carrylookahead_adder.o
-gtkwave carrylookahead_adder.vcd
+
+# iverilog -Wall -o tb_carrylookahead_adder.o       ../test_bench/tb_carrylookahead_adder.v
+# vvp tb_carrylookahead_adder.o
+# gtkwave carrylookahead_adder.vcd
 
 
 cd ..   # go back to project root directory
+
+# NOTE; remove all DUT files from dir. add one by one and make commits. same for TB files
