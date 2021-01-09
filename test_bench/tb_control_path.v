49cef3d65fffaa79880e7dacae79b09e263059fe
usmans1515 <usmansiddique1515@gmail.com>
2021-01-09 21:42:02 +0500

    updated control path TB

--- test_bench/tb_control_path.v
+++ test_bench/tb_control_path.v
@@ -1,3 +1,4 @@
+`include "../rtl/control_path.v"
 `timescale 1ns/100ps
 
 module tb_control_path;
@@ -5,35 +6,30 @@ module tb_control_path;
 localparam DATA_WIDTH=8;
 localparam T=2;
 localparam min='h0,max='hf;
-//-------------------------------------------------------------------------- ------------------- global signals
-  logic   clk;
-  logic   reset_n;
-//-------------------------------------------------------------------------- ------------------- matrix A i/o
-  logic   en_ReadMat_A;
-  logic   en_WriteMat_A;
-  logic   [3:0] rowAddr_A;
-  logic   [3:0] colAddr_A;
-  logic   [DATA_WIDTH-1:0] writeData_A;
-  logic   [DATA_WIDTH-1:0] readData_A;
-//-------------------------------------------------------------------------- ------------------- matrix B i/o
-  logic   en_ReadMat_B;
-  logic   en_WriteMat_B;
-  logic   [3:0] rowAddr_B;
-  logic   [3:0] colAddr_B;
-  logic   [DATA_WIDTH-1:0] writeData_B;
-  logic   [DATA_WIDTH-1:0] readData_B;
-//-------------------------------------------------------------------------- ------------------- data path i/o
-  logic   en_Mux;
-  logic   en_PPReg;
-  logic   en_FDReg;
-//-------------------------------------------------------------------------- ------------------- matrix C i/o
-  logic   en_ReadMat_C;
-  logic   en_WriteMat_C;
-  logic   [3:0] rowAddr_C;
-  logic   [3:0] colAddr_C;
-  logic   [DATA_WIDTH-1:0] writeData_C;
-  logic   [DATA_WIDTH-1:0] readData_C;
-//-------------------------------------------------------------------------- ------------------- 
+//-------------------------------------------------------------------------- global signals
+  reg   clk;
+  reg   reset_n;
+//-------------------------------------------------------------------------- matrix A i/o
+  wire  en_ReadMat_A;
+  wire  en_WriteMat_A;
+  wire  [3:0] rowAddr_A;
+  wire  [3:0] colAddr_A;
+//-------------------------------------------------------------------------- matrix B i/o
+  wire  en_ReadMat_B;
+  wire  en_WriteMat_B;
+  wire  [3:0] rowAddr_B;
+  wire  [3:0] colAddr_B;
+//-------------------------------------------------------------------------- data path i/o
+  wire  en_Mux;
+  wire  en_PPReg;
+  wire  en_FDReg;
+//-------------------------------------------------------------------------- matrix C i/o
+  wire  en_ReadMat_C;
+  wire  en_WriteMat_C;
+  wire  [3:0] rowAddr_C;
+  wire  [3:0] colAddr_C;
+
+//-------------------------------------------------------------------------- DUT instantiated
 control_path CONTROL_PATH(
 	.clk           (clk           ),
   .reset_n       (reset_n       ),
@@ -41,28 +37,25 @@ control_path CONTROL_PATH(
   .en_WriteMat_A (en_WriteMat_A ),
   .rowAddr_A     (rowAddr_A     ),
   .colAddr_A     (colAddr_A     ),
-  .writeData_A   (writeData_A   ),
-  .readData_A    (readData_A    ),
   .en_ReadMat_B  (en_ReadMat_B  ),
   .en_WriteMat_B (en_WriteMat_B ),
   .rowAddr_B     (rowAddr_B     ),
   .colAddr_B     (colAddr_B     ),
-  .writeData_B   (writeData_B   ),
-  .readData_B    (readData_B    ),
   .en_Mux        (en_Mux        ),
   .en_PPReg      (en_PPReg      ),
   .en_FDReg      (en_FDReg      ),
   .en_ReadMat_C  (en_ReadMat_C  ),
   .en_WriteMat_C (en_WriteMat_C ),
   .rowAddr_C     (rowAddr_C     ),
-  .colAddr_C     (colAddr_C     ),
-  .writeData_C   (writeData_C   ),
-  .readData_C    (readData_C    )
+  .colAddr_C     (colAddr_C     )
 );
 
+//-------------------------------------------------------------------------- clock generator
 always begin
   #(T/2) clk=1'b0;    #(T/2) clk=1'b1;
 end
+
+//-------------------------------------------------------------------------- main loop
 initial begin
   repeat(5) @(posedge clk)
     reset_n=1'b0;
@@ -71,4 +64,11 @@ initial begin
   $finish;
 end
 
+//-------------------------------------------------------------------------- vcd output
+initial begin
+	$dumpfile("control_path.vcd");
+	$dumpvars(0,CONTROL_PATH);
+end
+
+//-------------------------------------------------------------------------- end of code
 endmodule
